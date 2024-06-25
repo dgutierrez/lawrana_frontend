ca078bbcad266f19c3f9cf6882f8c81a0c1d802f
Diego Gutierrez da Silva <diego.gutierrezdasilva@gmail.com>
2024-06-24 21:35:18 -0300

    nome e foto emp

--- front/lawrana/src/app/pages/empresa/emp-home/emp-home.component.ts
+++ front/lawrana/src/app/pages/empresa/emp-home/emp-home.component.ts
@@ -1,7 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { NavigatorComponent } from '../../../shared/navigator/navigator.component';
 import { ViewerComponent } from '../../../shared/viewer/viewer.component';
 import { MenuItem } from '../../../shared/meu-sidenav/meu-sidenav.component';
+import { EmpresaService } from '../../../core/services/empresa.service';
+import { Empresa } from '../../../interfaces/empresa';
+import { response } from 'express';
+import { PerfilUsuario } from '../../../interfaces/usuario';
 
 @Component({
   selector: 'app-emp-home',
@@ -10,7 +14,7 @@ import { MenuItem } from '../../../shared/meu-sidenav/meu-sidenav.component';
   templateUrl: './emp-home.component.html',
   styleUrl: './emp-home.component.css'
 })
-export class EmpHomeComponent {
+export class EmpHomeComponent implements OnInit {
   usuarioMenuItems = signal<MenuItem[]>([
     {
       icon: 'source_environment',
@@ -33,4 +37,54 @@ export class EmpHomeComponent {
       route: 'dash'
     }
   ])
+
+  empresa: Empresa = {
+    configuracoes: {
+      claude_token: '',
+      disponibiliza_pasta_empresa: false,
+      disponibiliza_token_empresa: false,
+      gemini_token: '',
+      openai_token: '',
+      tamanho_armazenamento: 1,
+      url_status_usuario: ''
+    },
+    data_cadastro: '',
+    email: '',
+    foto: null,
+    foto_login: null,
+    nome: 'emp',
+    user: ''
+  }
+
+  perfilUser: PerfilUsuario = {
+    codigo_usuario: '',
+    codigo_usuario_integracao: '',
+    configuracoes:  {
+      openai_token: '',
+      gemini_token: ''
+    },
+    data_cadastro: '',
+    email: '',
+    foto: null,
+    nome: 'teste'
+  }
+
+  constructor(private empresaService: EmpresaService) {
+
+  }
+
+  ngOnInit(): void {
+    console.log('carregando emp')
+    this.empresaService.buscarEmpresa().subscribe((response) => {
+      this.empresa = response;
+
+      this.perfilUser.nome = this.empresa.nome;
+      this.perfilUser.foto = this.empresa.foto_login;
+
+      console.log('Empresa = ')
+      console.log(this.empresa.nome)
+      console.log('perfilUser = ')
+      console.log(this.perfilUser.nome)
+    });
+  }
 }
